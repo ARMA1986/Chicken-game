6e2df028305e58e4461169fc1227181aa67f5119
Unknown <alex.pcc.hm@gmail.com>
2017-10-23 20:20:47 -0600

    Oct 23.2017

---
+++ Chicken-Game/Assets/Scripts/ChickenMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMove : MonoBehaviour {
+
+	public Rigidbody enemy;
+
+	public float moveSpeed;
+
+	public Transform target;
+
+	public Transform target2;
+
+
+
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered chicken's trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+		if(other.gameObject.name == "Wolf"){
+			Debug.Log("Wolf has entered chicken's trigger");
+			transform.LookAt(target2);
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+		}
+	}
+	// // Use this for initialization
+	// void Start () {
+		
+	// }
+	
+	// // Update is called once per frame
+	// void Update () {
+		
+	// }
+}
---
+++ Chicken-Game/Assets/Scripts/WolfMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMove : MonoBehaviour {
+
+	public Rigidbody enemy;
+
+	public float moveSpeed;
+
+	public Transform target;
+
+	public Transform target2;
+
+
+
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered wolf's trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+
+		if(other.gameObject.name == "Chicken"){
+			Debug.Log("Chicken has entered wolf's trigger");
+			transform.LookAt(target2);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+}
