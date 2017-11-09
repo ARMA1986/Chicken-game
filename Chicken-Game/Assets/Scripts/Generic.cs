12bc9925e209ba997843e2444065936d8a6b3763
Unknown <alex.pcc.hm@gmail.com>
2017-11-08 20:16:27 -0700

    Nov 8th

--- Chicken-Game/Assets/Scripts/Generic.cs
+++ Chicken-Game/Assets/Scripts/Generic.cs
@@ -1,47 +1,82 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
 
-public class Generic : MonoBehaviour {
+// public class Generic : MonoBehaviour {
 
-// Position variables: 
-// public float posX=1.5f;
-// public float posY=1.5f;
-// public float posZ;
+// // // Position variables: 
+// // public float posX=1.5f;
+// // public float posY=1.5f;
+// // public float posZ=1.5f;
 
-public Vector3 pos;
+// // // public Vector3 pos;
 
+// // // Rotate variables
 
+// // // public float rotX;
+// // // public float rotY;
+// // // public float rotZ;
+// // // public float rotW;
 
-// Rotate variables
+// // // public Quaternion rot;
 
-// public float rotX;
-// public float rotY;
-// public float rotZ;
-// public float rotW;
+// // // Scale variables:
 
-public Quaternion rot;
+// // // public float x;
+// // // public float y;
+// // // public float z;
 
-// Scale variables:
+// // // public Vector3 scale;
 
-// public float x;
-// public float y;
-// public float z;
+// // public float moveSpeed=5f;
+// // public float turnSpeed=5f; 	
+// // void Start(){
+// // transform.position = new Vector3(posX,posY,posZ);
+// // }
 
-public Vector3 scale;
-
-void Update(){
-		// transform.position = new Vector3(posX,posY,posZ);
-		// transform.rotation = new Quaternion(rotX,rotY,rotZ,rotW);
-		// transform.localScale = new Vector3(x,y,z); 
+// // void Update(){
+// // 		// transform.position = new Vector3(posX,posY,posZ);
+// // 		// transform.rotation = new Quaternion(rotX,rotY,rotZ,rotW);
+// // 		// transform.localScale = new Vector3(x,y,z); 
 		
-		// Simplified way replacing the code above:
+// // 		// Simplified way replacing the code above:
 
-		transform.translate = pos;
-		transform.rotate = rot;
-		transform.localScale = scale;
-	}
+// // 		// transform.position = pos;
+// // 		// transform.rotation = rot;
+// // 		// transform.localScale = scale;
+
+// // 		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+// // 		transform.Rotate(Vector3.right, turnSpeed * Time.deltaTime);
+
+// // 		// transform.localPosition = transform.localPosition + -transform.forward 
+// // 		// * moveSpeed * Time.deltaTime;
+// // 	}
 	
 
-}
+// // }
+//  var noTarget = true;
+//  var qTo = Quaternion;
+//  var speed = 1.25;
+//  var rotateSpeed = 3.0;
+//  public timer = 0.0f;
+ 
+//  void Start() {
+//    qTo = Quaternion.Euler(Vector3(0.0,Random.Range(-180.0, 180.0), 0.0));
+ 
+//  }
+ 
+//  void Update() {
+ 
+//      timer += Time.deltaTime;
+ 
+//      if(noTarget == true) {//when not targeting hero     
+//          if(timer > 2) { // timer resets at 2, allowing .5 s to do the rotating
+//                qTo = Quaternion.Euler(Vector3(0.0,Random.Range(-180.0, 180.0), 0.0));  
+//                timer = 0.0;
+//              }
+//          transform.rotation = Quaternion.Slerp(transform.rotation, qTo, Time.deltaTime * rotateSpeed);
+//          transform.Translate(Vector3.forward * speed * Time.deltaTime);
+//      }
+//  }
+// }
---
+++ Chicken-Game/Assets/Scripts/Wander.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+public float moveSpeed=1f;
+
+// Gotta think about behaviors for the Wander function
+void MoveForward(){
+	// transform.Translate(Vector3(0,0,moveSpeed)); OR
+
+	transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+
+}
+
+void Turn(){
+	// int randomY will create a randomn number
+	// the Random.Range() will take parameters between one number and another
+	int randomY= Random.Range(0,360);
+//  transform.Rotate takes as parameters the x,y,z values in that order
+	transform.Rotate(0,randomY,0);
+}
+
+void OnTriggerStay(Collider other){
+	if (other.gameObject.tag== "CheckPoint"){
+		Turn();
+	}
+	else{
+		MoveForward();
+	}
+}
+
+}
